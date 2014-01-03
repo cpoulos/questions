bf1779d234777154b3754bc52b3429ea17505423
Chris Poulos <poulosc1@gmail.com>
2014-01-03 00:02:55 -0500

    Initialized Git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Superlatif::Application.config.secret_key_base = '7c85a8842a6c9d9526aa6d18ef7777328ccdfbc6acbbe5aeeb40c6efa4dde203ba4d1d9396750547b0541568e73734f99fb6e73eb9a6f70a77fd5efe72670f71'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Superlatif::Application.config.session_store :cookie_store, key: '_superlatif_session'
